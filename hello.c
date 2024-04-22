dcff94c61d56ebce218740f83cdae55d071eef59
fuyi-atlas <thread.zhou@gmail.com>
2024-04-22 21:14:03 +0800

    add hello case

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a,b;
+    float c;
+    printf("hello world\n");
+    printf("a: %d, b: %d\n", a, b);
+    printf("c: %f", c);
+    printf("\a");
+    return 0;
+}

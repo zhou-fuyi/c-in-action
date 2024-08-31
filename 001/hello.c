c879f61b46227548e86d31ccf9f0aa57fbdecdab
fuyi-atlas <thread.zhou@gmail.com>
2024-08-31 11:32:28 +0800

    update

---
+++ 001/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a,b;
+    float c;
+    printf("hello world\n");
+    printf("12+34=%d\n", 12+34);
+    printf("%d\n", (2+2*(2*2-2)%2/3));
+    return 0;
+}
